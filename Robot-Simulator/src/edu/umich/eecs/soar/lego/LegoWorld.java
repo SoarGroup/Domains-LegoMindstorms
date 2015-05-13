2ffab2a62eb7c08d68903ba5596b7f9729ee94b4
Alex Turner <bluechill@me.com>
2015-05-13 12:34:52 -0400

    Switched paths to be better.  Also added eclipse variables for soar home and src location.

--- Robot-Simulator/src/edu/umich/eecs/soar/lego/LegoWorld.java
+++ Robot-Simulator/src/edu/umich/eecs/soar/lego/LegoWorld.java
@@ -6,8 +6,8 @@ import edu.umich.eecs.soar.lego.core.LegoCore;
 
 public class LegoWorld
 {
-	private static final String DiscreteLegoWorldPath = "/Users/bluechill/Desktop/DiscreteLegoWorld/src/edu/umich/eecs/soar/lego/";
-	private static final String SoarDebuggerJarPath = "/Users/bluechill/Developer/SoarGroupProjects/Soar/master/out/SoarJavaDebugger.jar";
+	private static final String DiscreteLegoWorldPath = System.getenv("PROJECT_LOC") + "/src/edu/umich/eecs/soar/lego/";
+	private static final String SoarDebuggerJarPath = System.getenv("SOAR_HOME") + "/SoarJavaDebugger.jar";
 	private static final String AgentFile = "Stage4-completed";
 	private static final String Map = "Map1";
 	private static final Integer seed = new Integer((int)(Math.random() * 10000.0));
